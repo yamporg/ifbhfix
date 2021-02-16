efe6896ac0a8f744883c06aeaaf07aa2c3da0a6f
Ivan Trubach <mr.trubach@icloud.com>
2021-02-16 01:23:56 +0000

    Revert "Trying to make things work in prod env"
    This reverts commit 778c8e74baeb2674d4c18ef7168ee1d5a6c7413c.

--- src/main/java/io/github/yamporg/ifbhfix/mixins/MixinBlackHoleTankBlock.java
+++ src/main/java/io/github/yamporg/ifbhfix/mixins/MixinBlackHoleTankBlock.java
@@ -4,27 +4,19 @@ import com.buuz135.industrial.tile.block.BlackHoleTankBlock;
 import io.github.yamporg.ifbhfix.FluidHandlerWrapper;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.capability.templates.FluidHandlerItemStack;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.util.Constants;
 
-@Mixin(value = BlackHoleTankBlock.class)
+@Mixin(value = BlackHoleTankBlock.TankCapabilityProvider.class, remap = false)
 public abstract class MixinBlackHoleTankBlock {
-    @Mixin(value = BlackHoleTankBlock.TankCapabilityProvider.class)
-    public static abstract class MixinTankCapabilityProvider {
-        @Mutable
-        @Final
-        @Shadow(remap = false)
-        public FluidHandlerItemStack fluidHandlerItemStack;
+    @Shadow public FluidHandlerItemStack fluidHandlerItemStack;
 
-        @Inject(method = Constants.CTOR, at = @At("RETURN"))
-        public void init(BlackHoleTankBlock outer, ItemStack itemStack, CallbackInfo ci) {
-            this.fluidHandlerItemStack = new FluidHandlerWrapper(itemStack, Integer.MAX_VALUE);
-        }
+    @Inject(method = Constants.CTOR, at = @At("RETURN"))
+    public void init(BlackHoleTankBlock outer, ItemStack itemStack, CallbackInfo ci) {
+        this.fluidHandlerItemStack = new FluidHandlerWrapper(itemStack, Integer.MAX_VALUE);
     }
 }
