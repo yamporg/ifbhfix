778c8e74baeb2674d4c18ef7168ee1d5a6c7413c
Ivan Trubach <mr.trubach@icloud.com>
2021-02-16 03:48:02 +0300

    Trying to make things work in prod env

--- src/main/java/io/github/yamporg/ifbhfix/mixins/MixinBlackHoleTankBlock.java
+++ src/main/java/io/github/yamporg/ifbhfix/mixins/MixinBlackHoleTankBlock.java
@@ -4,19 +4,27 @@ import com.buuz135.industrial.tile.block.BlackHoleTankBlock;
 import io.github.yamporg.ifbhfix.FluidHandlerWrapper;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.capability.templates.FluidHandlerItemStack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.util.Constants;
 
-@Mixin(value = BlackHoleTankBlock.TankCapabilityProvider.class, remap = false)
+@Mixin(value = BlackHoleTankBlock.class)
 public abstract class MixinBlackHoleTankBlock {
-    @Shadow public FluidHandlerItemStack fluidHandlerItemStack;
+    @Mixin(value = BlackHoleTankBlock.TankCapabilityProvider.class)
+    public static abstract class MixinTankCapabilityProvider {
+        @Mutable
+        @Final
+        @Shadow(remap = false)
+        public FluidHandlerItemStack fluidHandlerItemStack;
 
-    @Inject(method = Constants.CTOR, at = @At("RETURN"))
-    public void init(BlackHoleTankBlock outer, ItemStack itemStack, CallbackInfo ci) {
-        this.fluidHandlerItemStack = new FluidHandlerWrapper(itemStack, Integer.MAX_VALUE);
+        @Inject(method = Constants.CTOR, at = @At("RETURN"))
+        public void init(BlackHoleTankBlock outer, ItemStack itemStack, CallbackInfo ci) {
+            this.fluidHandlerItemStack = new FluidHandlerWrapper(itemStack, Integer.MAX_VALUE);
+        }
     }
 }
--- src/main/java/io/github/yamporg/ifbhfix/mixins/MixinBlackHoleUnitBlock.java
+++ src/main/java/io/github/yamporg/ifbhfix/mixins/MixinBlackHoleUnitBlock.java
@@ -4,19 +4,27 @@ import com.buuz135.industrial.tile.block.BlackHoleUnitBlock;
 import io.github.yamporg.ifbhfix.ItemHandlerWrapper;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.util.Constants;
 
-@Mixin(value = BlackHoleUnitBlock.StorageItemHandler.class, remap = false)
+@Mixin(BlackHoleUnitBlock.class)
 public abstract class MixinBlackHoleUnitBlock {
-    @Shadow private IItemHandler itemHandler;
+    @Mixin(value = BlackHoleUnitBlock.StorageItemHandler.class)
+    public static abstract class MixinStorageItemHandler {
+        @Mutable
+        @Final
+        @Shadow(remap = false)
+        private IItemHandler itemHandler;
 
-    @Inject(method = Constants.CTOR, at = @At("RETURN"))
-    public void init(BlackHoleUnitBlock outer, ItemStack itemStack, CallbackInfo ci) {
-        this.itemHandler = new ItemHandlerWrapper(itemStack, this.itemHandler);
+        @Inject(method = Constants.CTOR, at = @At("RETURN"))
+        public void init(BlackHoleUnitBlock outer, ItemStack itemStack, CallbackInfo ci) {
+            this.itemHandler = new ItemHandlerWrapper(itemStack, this.itemHandler);
+        }
     }
 }
